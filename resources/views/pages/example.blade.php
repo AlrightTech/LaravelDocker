01e897a5eb634f19aa46c6073f50e560672b646e
Muhammad Saad <63445891+saadjavaid67@users.noreply.github.com>
2024-10-02 18:45:20 +0500

    init

---
+++ resources/views/pages/example.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        {{-- Your code goes here --}}
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/{page}', function ($page) {
+    return view('pages.' . $page);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

fdf64acad3dcf1c4bb277809ba84cd3c9f686a24
nibrvs <nibrvss@icloud.com>
2024-10-03 19:09:27 +0500

    fgfgfgfgf

---
+++ resources/views/pages/nibras.blade.php
@@ -0,0 +1,55 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Rethink Sans' rel='stylesheet'>
+
+</head>
+
+<body>
+    <div class="w-100 h-100 d-flex align-items-center py-5 background" style="background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(112,9,121,1) 45%, rgba(0,212,255,1) 100%);">
+
+
+
+        <section
+            class="container d-flex flex-column align-items-center justify-content-center gap-3 bg-light w-75 rounded-4 m-auto bg-opacity-25 text-light"
+            style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; height: 500px;">
+            <img src="{{asset('assets/images/pfpicon.png')}}" alt="pfp" style="width: 100px;">
+            <h1>Nibras Khan</h1>
+            <span>Front-end Developer</span>
+            <a class="pointer text-decoration-none text-light">nibrvss@icloud.com</a>
+            <span class="w-50 text-center p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic velit, iure
+                totam omnis magnam dicta fugit
+                quia necessitatibus animi labore modi libero maiores. Similique eos, voluptatem vel corporis vitae
+                sapiente
+            </span>
+
+            <div class="d-flex align-items-center gap-4">
+                <i class="fa-brands fa-instagram pointer"></i>
+                <i class="fa-brands fa-github pointer"></i>
+                <i class="fa-brands fa-linkedin pointer" ></i>
+                <i class="fa-brands fa-x-twitter pointer"></i>
+            </div>
+
+        </section>
+
+
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/bced75b46a.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/{page}', function ($page) {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/index', function () {
+    return view('pages.index' );
+});

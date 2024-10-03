3fb79ba932ebbc56bac1cf480fec147d8d11b79f
naveed <naveedzafar123abx@gmail.com>
2024-10-03 18:00:13 +0500

    I made my portfolio

---
+++ resources/views/pages/naveed_zafar.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <section class="personal-details mx-auto">
+            <div>
+                <img src="./naveepic.jpeg" alt="" class="w-100 rounded-4 profile-pic" />
+            </div>
+            <div>
+                <h1>Naveed Zafar</h1>
+                <div class="text-18-600">EMP-ID-<span class="id-no">358</span></div>
+                <div class="text-14-500">
+                    I am working as an intern in Alright TECH.I have worked on switch
+                    Ai,XO-black,Community plus and currently working on Shawzon.
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
